5124303c5747009bd00e1f37f6a9cc633a0697b2
sanjayk <sanjaykb0@protonmail.com>
2022-08-02 15:24:23 +0530

    Reworked Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,67 +1,39 @@
-
 import React from "react";
-import moment from "moment-timezone";
 import { Row, Col, Card, OverlayTrigger, Tooltip, Image, Button } from 'react-bootstrap';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCogs, faDownload, faRocket } from "@fortawesome/free-solid-svg-icons";
-import { Horse, Heart, Cube, Plus, CurrencyBtc } from "phosphor-react";
-import BS5Logo from "../assets/img/technologies/bootstrap-5-logo.svg";
-import ReactLogo from "../assets/img/technologies/react-logo.svg";
-import LaravelLogo from "../assets/img/technologies/laravel-logo.svg";
-import GitHubButton from 'react-github-btn';
 import { Link } from 'react-router-dom';
 import { Routes } from "../routes";
+import { Nav } from "react-bootstrap";
+import { House, MagnifyingGlass, Plus} from "phosphor-react";
 
-export default (props) => {
-  const currentYear = moment().get("year");
-  const showSettings = props.showSettings;
+const iconSize = 32;
 
-  const toggleSettings = (toggle) => {
-    props.toggleSettings(toggle);
-  }
+// Footer V.2. to look more like the bottom nav bar from the figma designs.
 
+export default () => {
   return (
-    <div>
-      {showSettings ? (
-        <Card className="theme-settings">
-          <Card.Body className="pt-4">
-            <Button className="theme-settings-close" variant="close" size="sm" aria-label="Close" onClick={() => { toggleSettings(false) }} />
-            <div className="d-flex justify-content-between align-items-center mb-3">
-             
-            </div>
-           
-            
-            <div className="d-flex justify-content-center">
-              <Card.Link >
-         <row>  
-      <button as={Link} to={Routes.CollectorInput.path}>Create a CGP cycle</button>
+    <Row className="bg-dark d-flex align-items-center justify-content-center bottom-align">
+      <Col lg={4} className="d-flex flex-row justify-content-center align-items-center">
+        <Nav defaultActionKey="home" variant="pills" className="d-flex flex-row ">
+          <Nav.Item>
+            <Nav.Link eventKey="home" href="#" className="mb-sm-3 mb-md-0">
+              <House size={iconSize}/>
+            </Nav.Link>
+          </Nav.Item>
 
-      {/* Buttons below need to be implemented! */}
-
-      <button>Update a CGP cycle</button>
-      <button>Upload a GST bill</button>
-        
-      </row>    
-    
-        
-              </Card.Link>
-
-            </div>
-          </Card.Body>
-        </Card>
-      ) : (
-        <Card className="theme-settings theme-settings-expand" onClick={() => { toggleSettings(true) }}>
-          <Card.Body className="p-3 py-2">
-            <span className="fw-bold h6"><Plus size={28} className="me-1 fs-7" /></span>
-          </Card.Body>
-        </Card>
-      )}
-      <footer className="footer section py-5">
-        <Row>
-          
-        
-        </Row>
-      </footer>
-    </div>
-  );
-};
+          <Nav.Item>
+            <Nav.Link eventKey="home" href="#" className="mb-sm-3 mb-md-0">
+              <Plus size={iconSize}/>
+            </Nav.Link>
+          </Nav.Item>
+ 
+          <Nav.Item>
+            <Nav.Link eventKey="home" href="#" className="mb-sm-3 mb-md-0">
+              <MagnifyingGlass size={iconSize} />
+            </Nav.Link>
+          </Nav.Item>
+ 
+        </Nav>
+      </Col>
+    </Row>
+  )
+}
