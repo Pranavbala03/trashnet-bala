51bf2687d94f745fa47bada1389a502bd40ad108
Pranavbala Ganesh <pranavb.ganesh@gmail.com>
2022-08-04 13:24:11 +0530

    removed everything from dashboard

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,29 +11,7 @@ const iconSize = 32;
 
 export default () => {
   return (
-    <Row className="bg-dark d-flex align-items-center justify-content-center bottom-align">
-      <Col lg={4} className="d-flex flex-row justify-content-center align-items-center">
-        <Nav defaultActionKey="home" variant="pills" className="d-flex flex-row ">
-          <Nav.Item>
-            <Nav.Link eventKey="home" href="#" className="mb-3">
-              <House size={iconSize}/>
-            </Nav.Link>
-          </Nav.Item>
-
-          <Nav.Item>
-            <Nav.Link eventKey="home" href="#" className="mb-3">
-              <Plus size={iconSize}/>
-            </Nav.Link>
-          </Nav.Item>
- 
-          <Nav.Item>
-            <Nav.Link eventKey="home" href="#" className="mb-3">
-              <MagnifyingGlass size={iconSize} />
-            </Nav.Link>
-          </Nav.Item>
- 
-        </Nav>
-      </Col>
-    </Row>
+    <>
+    </>
   )
 }
