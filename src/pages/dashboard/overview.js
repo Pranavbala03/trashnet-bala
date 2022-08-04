9f954367064c6f083f1090685c73886aa0b5dd0d
Pranavbala Ganesh <pranavb.ganesh@gmail.com>
2022-08-04 15:33:43 +0530

    butt-ons

--- src/pages/dashboard/overview.js
+++ src/pages/dashboard/overview.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Routes } from "../../routes";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { House, List, User } from "phosphor-react";
 
 class NavigationBar extends React.Component {
   render() {
     return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="dark" fixed="bottom">
       <Container>
        
         <Nav className="me-auto">
-          
+        <Button variant="dark">
           <Nav.Link href="#features"><List size={32} /></Nav.Link>
+     </Button>
+    <Button variant="dark">
+      <br></br>
           <Nav.Link href="#home"><House size={32} /></Nav.Link>
+          </Button>
+         <Button variant="dark">
           <Nav.Link href="#pricing"><User size={32} /></Nav.Link>
+          </Button>
         </Nav>
       </Container>
     </Navbar>
